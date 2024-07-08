14b3cfaf8c3b82fa6d76f318b8562232376e494e
Yaroslav Baranivskyy <ya.baranivskyi@ukma.edu.ua>
2024-07-08 18:25:41 +0300

    HomeWork4 has been uploaded.

---
+++ HomeWork4/Personal Page/src/app/mock-component2-2.ts
@@ -0,0 +1,9 @@
+
+import { Links } from "./Links";
+
+export const LINKS: Links[] = [
+  { id: 1, link: 'https://github.com/theronib' },
+  { id: 2, link: 'https://x.com/programming' },
+  { id: 3, link: 'https://facebook.com/web-design' },
+  { id: 4, link: 'https://linkedin.com/databases' }
+];
